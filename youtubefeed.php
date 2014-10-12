a99505ac444177c6ef1abc2ff0e42aaf329f4e58
Bernd Bestel <bernd@berrnd.de>
2014-10-12 15:08:25 +0200

    Added some caching to reduce YouTube API calls

--- youtubefeed.php
+++ youtubefeed.php
@@ -1,17 +1,24 @@
 <?php
 
 require_once 'config.php';
+require_once 'packages/PHP-SimpleCache/SimpleCache.php';
 
 function youtube_get_channel_by_name($channelName) {
+    $cache = new Gilbitron\Util\SimpleCache();
+    $cache->cache_time = 604800; //1 week
+
     $url = 'https://www.googleapis.com/youtube/v3/channels?part=id,snippet,contentDetails&forUsername=' . $channelName . '&key=' . YOUTUBE_API_KEY;
-    $response = file_get_contents($url);
+    $response = $cache->get_data("youtube_get_channel_by_name_$channelName", $url);
     $data = json_decode($response, true);
     return $data['items'][0];
 }
 
 function youtube_get_channel_by_id($channelId) {
+    $cache = new Gilbitron\Util\SimpleCache();
+    $cache->cache_time = 604800; //1 week
+
     $url = 'https://www.googleapis.com/youtube/v3/channels?part=id,snippet,contentDetails&id=' . $channelId . '&key=' . YOUTUBE_API_KEY;
-    $response = file_get_contents($url);
+    $response = $cache->get_data("youtube_get_channel_by_id_$channelId", $url);
     $data = json_decode($response, true);
     return $data['items'][0];
 }
