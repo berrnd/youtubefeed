7a2eb2cf56ad42c1d1f090e6b13faede9b5f9e27
Bernd Bestel <bernd@berrnd.de>
2015-03-05 19:34:40 +0100

    Added automatic cleanup of cache directory

--- youtubefeed.php
+++ youtubefeed.php
@@ -7,6 +7,8 @@ function youtube_get_channel_by_name($channelName) {
     $cache = new Gilbitron\Util\SimpleCache();
     $cache->cache_time = 604800; //1 week
 
+    clean_up_cache('youtube_get_channel_by_name_$channelName*', $cache->cache_time);
+
     $url = 'https://www.googleapis.com/youtube/v3/channels?part=id,snippet,contentDetails&forUsername=' . $channelName . '&key=' . YOUTUBE_API_KEY;
     $response = $cache->get_data("youtube_get_channel_by_name_$channelName", $url);
     $data = json_decode($response, true);
@@ -16,6 +18,8 @@ function youtube_get_channel_by_name($channelName) {
 function youtube_get_channel_by_id($channelId) {
     $cache = new Gilbitron\Util\SimpleCache();
     $cache->cache_time = 604800; //1 week
+
+    clean_up_cache('youtube_get_channel_by_id*', $cache->cache_time);
 
     $url = 'https://www.googleapis.com/youtube/v3/channels?part=id,snippet,contentDetails&id=' . $channelId . '&key=' . YOUTUBE_API_KEY;
     $response = $cache->get_data("youtube_get_channel_by_id_$channelId", $url);
@@ -34,6 +38,8 @@ function youtube_get_video($videoId) {
     $cache = new Gilbitron\Util\SimpleCache();
     $cache->cache_time = 4838400; //8 weeks
 
+    clean_up_cache('youtube_get_video*', $cache->cache_time);
+
     $url = 'https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id=' . $videoId . '&key=' . YOUTUBE_API_KEY;
     $response = $cache->get_data("youtube_get_video_$videoId", $url);
     $data = json_decode($response, true);
@@ -44,4 +50,16 @@ function xml_entities($string) {
     return str_replace(
             array("&", "<", ">", '"', "'"), array("&amp;", "&lt;", "&gt;", "&quot;", "&apos;"), $string
     );
+}
+
+function clean_up_cache($fileNamePattern, $time) {
+    $files = glob("cache/$fileNamePattern");
+    $now = time();
+
+    foreach ($files as $file) {
+        if (is_file($file) && basename($file) != 'index.html') {
+            if ($now - filemtime($file) >= $time)
+                unlink($file);
+        }
+    }
 }
